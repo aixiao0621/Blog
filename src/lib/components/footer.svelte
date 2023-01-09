121ccee8cc636e9db1fea7ae1967eb9bcd64f307
aixiao0621 <ymx0434@qq.com>
2023-01-09 22:37:01 +0800

    tttr

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -37,9 +37,6 @@
         href="https://github.com/importantimport/urara">
         Urara
       </a>
-		<br>
-		<a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0
-		</a>
       {#if footerConfig.html}
         <br />
         {@html footerConfig.html}
--- src/lib/config/friends.ts
+++ src/lib/config/friends.ts
@@ -25,57 +25,27 @@ export type Friend = {
 }
 export const friends: Friend[] = [
   {
-  id: 'kwaa',
-  rel: 'friend',
-  link: 'https://kwaa.dev',
-  html: `<div class="card w-screen max-w-[24rem] bg-base-100 bg-gradient-to-tr from-primary to-accent text-primary-content shadow-lg transition-shadow duration-500 hover:shadow-2xl">
-    <div class="absolute top-4 rotate-6 text-4xl font-bold leading-tight opacity-10">藍+85CD<br />./kwaa.dev</div>
-    <div class="card-body p-4">
-      <div class="flex items-center gap-4">
-        <div class="avatar mb-auto w-20 shrink-0">
-          <img class="rounded-xl" style="image-rendering:pixelated" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAVFBMVEUAAADW29T///+5wcc4ODjz8OZVVVX/7MkVFRXtrpqTJiVHKxPZOjr/+/QICz3/07d1TCNErbkaSXtgERn59vb//OPSzMzuuwKZne20srL0mIyWGwWygNgKAAAAAXRSTlMAQObYZgAAAOtJREFUKM9djwuSwyAMQ2vsBDBJyK/tfu5/z5W6aYZUMB7QQwZuhwIkwnq7KIgqTFQJV3/USQX+OoE0fqfLIh0kCtKC31VGEEGqBezdydixfAARnNeRKZDmClE4mFE/gAKpTjFOcgEde01fa4wrDjRAlGNf7vdF5QKEzSJUCRpftmFwrbW6vEGYXwEzHzi2BvCF1ve9/7hvDSgqBJYQeHxrA4oqAskA9ocpwUkIkrvbnppvlNnMUkn+9B4r+CdAAACVM5yJnBnA9U+8wCzncPgRynP6V+WGIQZyzMWspwzrTEASSi4F/kHCy/4DaDYJuEU/v5oAAAAASUVORK5CYII=" alt="藍#+85CD" />
-        </div>
-        <div class="card-title flex-1 flex-col items-end gap-0">
-          <span class="p-name text-right">藍+85CD</span>
-          <span class="text-right opacity-50">./kwaa.dev</span>
-        </div>
-      </div>
-      <div class="p-note prose opacity-70">ゴミ溜めで埋もれたまま、星空を眺めてるよ</div>
-    </div>
-  </div>`
-  },
-  {
-    id: 'test',
-    title: 'testtest',
-    link: 'https://kwaa.dev',
-    descr: 'testtqsatsatsartsa',
-    avatar: 'https://kwaa.dev/assets/any@512.webp'
-  },
-  {
-    id: 'test3',
-    title: 'testtest',
-    link: 'https://kwaa.dev',
-    descr: 'testtqsatsatsartsa',
-    avatar: 'https://kwaa.dev/assets/any@512.webp'
-  },
-  {
-    id: 'test4',
-    name: ':hatsunemiku: 藍 :hatsunemiku:',
-    title: '~/kwaa.moe',
-    link: 'https://kwaa.moe/@kwa',
-    descr: 'ゴミ溜めで埋もれたまま、星空を眺めてるよ',
-    avatar: 'https://kwaa.moe/media/975fc04911e242147be77b60b93839b6dd1a317112717562944e3c7aef1f0203.png'
+    id: 'id2',
+    name: 'StevenRCE0',
+    title: 'rcex.live',
+    link: 'https://rcex.live',
+    descr: '起风了，唯有努力试着生存',
+    avatar: 'https://c.rcex.live:8/IR/United@1x.png'
  },
- {
-   id: 'test5',
-   name: '藍',
-   title: '藍藍藍藍藍',
-   link: 'https://kwaa.dev',
-   descr: 'without avatar'
-  },
+   {
+    id: 'id3',
+    name: 'StevenRCE0',
+    title: 'rcex.live',
+    link: 'https://rcex.live',
+    descr: '起风了，唯有努力试着生存',
+    avatar: 'https://c.rcex.live:8/IR/Shape.svg'
+ },
   {
-    id: 'test6',
-    title: 'Test6',
-    name: 'test6'
+    id: 'id1',
+    name: 'Trdthg',
+    title: 'trdthg.github.io',
+    link: 'https://trdthg.github.io',
+    descr: 'Rustt translation team member.',
+    avatar: 'https://trdthg.github.io/img/logo_png.png'
   }
 ]
